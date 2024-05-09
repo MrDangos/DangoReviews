c19617883471cd829305d4c1124c220b1950035a
MrDangos <hrr4114715@gmail.com>
2024-05-08 21:57:35 -0500

    dialed in the layout
    dialed in the layout for the music reviews adding some more color for a bit more pop

---
+++ js/mainMusic.js
@@ -0,0 +1,7 @@
+// read in the musicFile
+// put songs into list and sort them from highest to loest dango score
+// display the top 5 songs with a                 
+// <li><a id = '${title}' href='${source}'> {title} by {artist} </a></li>
+// in an unorderd list
+
+
